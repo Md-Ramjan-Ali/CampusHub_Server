2fdbb2a4b0156953d0078389a8afeca23f8ccf58
ramjan-devs <gmramjanali100@gmail.com>
2026-07-10 21:55:56 +0600

    feat: implement Prisma module and environment configuration with Joi validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
+import configuration from './config/configuration';
+import { envValidationSchema } from './config/env.validation';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      cache: true,
+      load: [configuration],
+      validationSchema: envValidationSchema,
+      expandVariables: true,
+    }),
+  ],
 })
 export class AppModule {}
---
+++ src/config/env.validation.ts
@@ -0,0 +1,15 @@
+import * as Joi from 'joi';
+
+export const envValidationSchema = Joi.object({
+  PORT: Joi.number().default(5000),
+
+  NODE_ENV: Joi.string()
+    .valid('development', 'production', 'test')
+    .default('development'),
+
+  DATABASE_URL: Joi.string().required(),
+
+  JWT_SECRET: Joi.string().required(),
+
+  JWT_EXPIRES_IN: Joi.string().required(),
+});
